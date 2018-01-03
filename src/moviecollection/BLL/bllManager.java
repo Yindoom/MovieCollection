03f48ec7822854cbb02c150243ed0dc152d187fc
ZeXVex <32293921+ZeXVex@users.noreply.github.com>
2018-01-03 07:45:29 +0100

    BLL fix

--- src/moviecollection/BLL/bllManager.java
+++ src/moviecollection/BLL/bllManager.java
@@ -17,7 +17,6 @@ import moviecollection.DAL.dalManager;
  * @author ZeXVex
  */
 public class bllManager {
-<<<<<<< HEAD
     
     private ObservableList<Movies> Movies = FXCollections.observableArrayList(new ArrayList<>());
     
@@ -26,15 +25,9 @@ public class bllManager {
     public List<Movies> getAllMovies(){
         return DAL.getAllMovies();
     }
-
-=======
-dalManager DAL = new dalManager();
->>>>>>> 3f6716eaf4a3cd4bd71191dc3d13252e6a1615e3
+    
     public static void remove(Movies selectedMovies) {
        return DAL.remove(selectedMovies);
-    }    
-    public static void getAllMovies (dalManager getAllMovies) {
-       return DAL.getAllMovies();
     }
 }
 
