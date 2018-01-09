477dc4e4108ad8681c845d311de9e261a510eea3
Yindoom <yindoom@hotmail.com>
2018-01-09 09:01:27 +0100

    Initial commit

---
+++ src/moviecollection/MainWindowController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moviecollection;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author Yindo
+ */
+public class MainWindowController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private TableView<?> movieList;
+    @FXML
+    private TableView<?> categoryList;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addMovie(ActionEvent event) {
+    }
+
+    @FXML
+    private void editMovie(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteMovie(ActionEvent event) {
+    }
+
+    @FXML
+    private void showAllMovies(ActionEvent event) {
+    }
+    
+}
