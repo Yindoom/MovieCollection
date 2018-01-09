1c24ea7cb7bacf99f5118a79552b3bd91bbdcbda
Fabio <fabio_moniz_1@hotmail.com>
2018-01-09 10:29:39 +0000

    made the addmovie method

--- src/moviecollection/DAL/dalManager.java
+++ src/moviecollection/DAL/dalManager.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -48,6 +49,36 @@ public class dalManager {
                     Level.SEVERE, null, ex);
         }
         return allMovies;
+    }
+    
+    public void addMovies(Movies movie) {
+        try (Connection con = cm.getConnection()) {
+            String sql
+                    = "INSERT INTO Movie"
+                    + "(name, rating, filelink, lastview) "
+                    + "VALUES(?,?,?,?)";
+            PreparedStatement pstmt
+                    = con.prepareStatement(
+                            sql, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, movie.getName());
+            pstmt.setString(2, movie.getRating());
+            pstmt.setString(3, movie.getFileLink());
+            pstmt.setString(4, movie.getLastview());
+
+            int affected = pstmt.executeUpdate();
+            if (affected<1)
+                throw new SQLException("Song could not be added");
+
+            // Get database generated id
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                movie.setId(rs.getInt(1));
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(dalManager.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
     }
     
     public List<Category> getAllCategory() {
